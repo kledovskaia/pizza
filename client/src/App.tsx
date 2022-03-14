84f7ff9da47b78bbee719ccec670356c86f1f381
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-14 16:05:42 +0300

    templates of the pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import { Page } from './components/Page';
+import { Cart } from './pages/Cart';
+import { Home } from './pages/Home';
+
 export const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <Page>
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Page>
+  );
 };
