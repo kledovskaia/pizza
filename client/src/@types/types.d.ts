1669de61d1c0b14d3e224839d1f4d6c354b45903
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-16 11:22:45 +0300

    Pizza template

--- client/src/@types/types.d.ts
+++ client/src/@types/types.d.ts
@@ -1,1 +1,10 @@
 type GetArrayItemType<T extends any[]> = T extends (infer R)[] ? R : never;
+
+type TPizza = {
+  id: string;
+  name: string;
+  type: number[];
+  size: number[];
+  price: number;
+  imageUrl: string;
+};
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Page } from './components/Page';
 import { Cart } from './pages/Cart';
 import { Home } from './pages/Home';
 
 export const App = () => {
+  const [items, setItems] = useState<TPizza[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {
+        data: { pizzas },
+      } = await axios.get<{
+        pizzas: TPizza[];
+      }>('http://localhost:3000/db.json');
+      setItems(pizzas);
+    };
+    fetchData();
+  }, []);
+
   return (
     <Page>
       <Routes>
         <Route path="/cart" element={<Cart />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home items={items} />} />
       </Routes>
     </Page>
   );
