14202bf9903397ac5be3f1fa1ccc69d95be91b56
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-18 20:25:10 +0300

    getting pizzas from backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import { gql, useQuery } from '@apollo/client';
 import { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
@@ -7,18 +7,29 @@ import { Cart } from './pages/Cart';
 import Home from './pages/Home';
 import { setPizzas } from './redux/slices/pizzas';
 
+const GET_PIZZAS = gql`
+  query getPizzas($sortBy: Int, $filter: Int, $order: String) {
+    getPizzas(sortBy: $sortBy, filter: $filter, order: $order) {
+      id
+      imageUrl
+      name
+      types
+      sizes
+      price
+      category
+      rating
+    }
+  }
+`;
 type Props = typeof actions;
 
 const App: FC<Props> = ({ setPizzas }) => {
+  const { data, loading, error } = useQuery(GET_PIZZAS);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get<{
-        pizzas: TPizza[];
-      }>('http://localhost:3000/db.json');
-      setPizzas(data.pizzas);
-    };
-    fetchData();
-  }, []);
+    if (!data) return;
+    setPizzas(data.getPizzas);
+  }, [data]);
 
   return (
     <Page>
@@ -30,5 +41,8 @@ const App: FC<Props> = ({ setPizzas }) => {
   );
 };
 
-const actions = { setPizzas };
+const actions = {
+  setPizzas,
+};
+
 export default connect(null, actions)(App);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -6,13 +7,22 @@ import App from './App';
 import { store } from './redux/store';
 import './scss/app.scss';
 
+const uri = 'http://localhost:4000';
+
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
