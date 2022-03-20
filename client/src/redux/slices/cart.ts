8417806f321e8a3d1967fe61af48eda5e98c28ca
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-20 07:16:50 +0300

    refactoring

--- client/src/redux/slices/cart.ts
+++ client/src/redux/slices/cart.ts
@@ -18,7 +18,13 @@ const cart = createSlice({
   name: 'cart',
   reducers: {
     addPizza: (state, action: PayloadAction<TCartPizza>) => {
-      const pizza = state.value.pizzas[action.payload.id]?.items?.find(
+      if (!state.value.pizzas[action.payload.id])
+        state.value.pizzas[action.payload.id] = {
+          items: [],
+          count: 0,
+        };
+      const pizzaGroup = state.value.pizzas[action.payload.id];
+      const pizza = pizzaGroup?.items?.find(
         (pizza) =>
           pizza.size === action.payload.size &&
           pizza.type === action.payload.type
@@ -26,56 +32,50 @@ const cart = createSlice({
       if (pizza) {
         pizza.subTotal += pizza.price;
         pizza.count++;
-        state.value.pizzas[action.payload.id].count++;
+        pizzaGroup.count++;
         state.value.totalCount++;
         state.value.totalPrice += action.payload.price;
       } else {
-        state.value.pizzas[action.payload.id] = {
-          items: [
-            ...(state.value.pizzas[action.payload.id]?.items || []),
-            {
-              ...action.payload,
-              subTotal: action.payload.price,
-              count: 1,
-            },
-          ],
-          count: (state.value.pizzas[action.payload.id]?.count || 0) + 1,
-        };
+        pizzaGroup.items.push({
+          ...action.payload,
+          subTotal: action.payload.price,
+          count: 1,
+        });
+        pizzaGroup.count = (pizzaGroup?.count || 0) + 1;
         state.value.totalCount++;
         state.value.totalPrice += action.payload.price;
       }
     },
     removePizza: (state, action: PayloadAction<TCartPizza>) => {
-      const index = state.value.pizzas[action.payload.id]?.items.findIndex(
+      const pizzas = state.value.pizzas[action.payload.id]?.items;
+      const index = pizzas.findIndex(
         (pizza) =>
           pizza.size === action.payload.size &&
           pizza.type === action.payload.type
       );
-      const count = state.value.pizzas[action.payload.id]?.items[index].count;
+      const count = pizzas[index].count;
 
       if (count > 1) {
-        const pizza = state.value.pizzas[action.payload.id]?.items[index];
+        const pizza = pizzas[index];
         pizza.subTotal -= pizza.price;
         pizza.count--;
       } else {
-        state.value.pizzas[action.payload.id]?.items.splice(index, 1);
+        pizzas.splice(index, 1);
       }
       state.value.pizzas[action.payload.id].count--;
       state.value.totalCount--;
       state.value.totalPrice -= action.payload.price;
     },
     removeAllPizzas: (state, action: PayloadAction<TCartPizza>) => {
-      const index = state.value.pizzas[action.payload.id]?.items?.findIndex(
+      const pizzas = state.value.pizzas[action.payload.id].items;
+      const index = pizzas.findIndex(
         (pizza) =>
           pizza.size === action.payload.size &&
           pizza.type === action.payload.type
       );
-      state.value.totalCount -=
-        state.value.pizzas[action.payload.id].items[index].count;
-      state.value.totalPrice -=
-        state.value.pizzas[action.payload.id].items[index].subTotal;
-      state.value.pizzas[action.payload.id].count -=
-        state.value.pizzas[action.payload.id].items[index].count;
+      state.value.totalCount -= pizzas[index].count;
+      state.value.totalPrice -= pizzas[index].subTotal;
+      state.value.pizzas[action.payload.id].count -= pizzas[index].count;
 
       state.value.pizzas[action.payload.id]?.items?.splice(index, 1);
     },
