d21af35ab9998ebf1d1698470f2e04c00436fe78
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-21 13:43:03 +0300

    getting data from remote server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,6 @@ import { store } from './redux/store';
 import './scss/app.scss';
 
 const uri = process.env.REACT_APP_API_URI;
-// const uri = 'https://pizza-2022.herokuapp.com/api';
 
 const client = new ApolloClient({
   uri,
